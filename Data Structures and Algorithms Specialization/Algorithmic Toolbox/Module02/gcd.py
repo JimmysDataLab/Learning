5773eba7f3b34b5b716f000303dcdb5d5b25c568
Akhil George <akhil@Akhils-MacBook-Pro.local>
2024-10-24 00:58:08 +0530

    adding gcd

--- Data Structures and Algorithms Specialization/Algorithmic Toolbox/Module02/gcd.py
+++ Data Structures and Algorithms Specialization/Algorithmic Toolbox/Module02/gcd.py
@@ -1,1 +1,13 @@
 # Greatest Common Divisor 
+
+arr = list(map(int,input().split(' ')))
+root = min(arr)
+gcd = [None for _ in range(len(arr))]
+
+
+for id,num in enumerate(arr):
+        for i in range(2,root+1):
+            if num%i == 0 and root%i == 0:
+                gcd[id] = i
+print(arr)
+print(gcd)

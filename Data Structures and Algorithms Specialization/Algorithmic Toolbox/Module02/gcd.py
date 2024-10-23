6683fdca59f09970d633851ea2c523f3174a0da7
Akhil George <akhil@Akhils-MacBook-Pro.local>
2024-10-24 01:28:58 +0530

    finished gcd euclid

--- Data Structures and Algorithms Specialization/Algorithmic Toolbox/Module02/gcd.py
+++ Data Structures and Algorithms Specialization/Algorithmic Toolbox/Module02/gcd.py
@@ -1,13 +1,27 @@
 # Greatest Common Divisor 
 
+def gcd_naive(arr):
+    gcd = 1
+    for i in range(2,arr[-1]+1):
+        if arr[-1]%i == 0 and arr[0]%i == 0:
+            gcd = max(gcd,i)
+    return gcd
+
+def gcd_euclid(arr):
+
+    if arr[-1] == 0:
+        return arr[0]
+    
+    else:
+        return gcd_euclid([arr[-1],arr[0]%arr[-1]])
+
+
 arr = list(map(int,input().split(' ')))
-root = min(arr)
-gcd = [None for _ in range(len(arr))]
+arr.sort(reverse=True)
+gcd_euclid = gcd_euclid(arr)
+gcd_naive = gcd_naive(arr)
+
+print(gcd_naive,gcd_euclid)
 
 
-for id,num in enumerate(arr):
-        for i in range(2,root+1):
-            if num%i == 0 and root%i == 0:
-                gcd[id] = i
-print(arr)
-print(gcd)
+
